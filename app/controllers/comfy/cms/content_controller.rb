d6ba086bf1b4aed32e3b5b2ab45679c863d21a68
Jesse J. Wright <jjwright55@mac.com>
2015-04-02 07:48:54 -0700

    fix for youtube removal

--- app/controllers/comfy/cms/content_controller.rb
+++ app/controllers/comfy/cms/content_controller.rb
@@ -71,7 +71,6 @@ protected
       endidx = html.index('<', idx)
       image_str = html[(idx+1)..(endidx-1)]
       image_str.strip!
-      image_str.gsub!(',', ' ')
       imgs = image_str.split(' ')
       if imgs.size == 0
         html = remove_section(html, '<section id="thumb-gallery"', '</section>')
@@ -83,7 +82,10 @@ protected
           html.sub!("#{img}", "<li class=\"mTSThumbContainer\"><a rel=\"group1\" class=\"single_image\" href=\"#{img}\"><img  class=\"mTSThumb\" src=\"#{thumb}\"/></a></li>")
         end
       end
-
+      idx = html.index('nss_images')
+      endidx = html.index("</ul>", idx)
+      html[idx..endidx].gsub!(',', '')
+           
       # categories or tags
       tagstr = tags.join(" ")
       html.gsub!('$TAGS$', "#{tagstr}")

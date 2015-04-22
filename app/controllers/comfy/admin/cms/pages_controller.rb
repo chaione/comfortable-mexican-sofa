bfaa4d2cb7efab7fa0fa41881ad6c0903f2d71ac
Jesse J. Wright <jjwright55@mac.com>
2015-04-22 09:09:28 -0700

    minor fixes & refactor

--- app/controllers/comfy/admin/cms/pages_controller.rb
+++ app/controllers/comfy/admin/cms/pages_controller.rb
@@ -1,3 +1,4 @@
+require 'convert_page'
 class Comfy::Admin::Cms::PagesController < Comfy::Admin::Cms::BaseController
 
   before_action :check_for_layouts, :only => [:new, :edit]
@@ -81,79 +82,10 @@ class Comfy::Admin::Cms::PagesController < Comfy::Admin::Cms::BaseController
 protected
 
 
-def remove_section(html, first, last)
-  idx = html.index(first)
-  return html if idx.nil?
-  endidx = html.index(last, idx) + last.size
-  html = "#{html[0..(idx-1)]}#{html[endidx..(html.size)]}"
-end
-def render_page(cms_page)
-    # categories to tabs
-    tags_before = cms_page.categories.map(&:label)
-    tags = []
-    tags_before.each do |tag|
-      tags << tag if not tag =~ /location/
-    end
-    
-    # html
-    html = cms_page.content_cache
-    
-    # youtube
-    idx = html.index('<div class="video-container">')
-    endidx = html.index('</div>', idx) + '</div>'.size
-    srcidx = html.index('src="', idx) + 'src="'.size
-    endsrcidx = html.index('"', srcidx)
-    if endsrcidx - srcidx < 7
-      html = remove_section(html, '<div class="video-container">', '</div>')
-    end
-    
-    # content
-    idx = html.index('<div class="body_content">') + '<div class="body_content">'.size
-    endidx = html.index('</div>', idx)
-    body = html[(idx+1)..(endidx-1)]
-    body.gsub!("\r\n\r\n", "<p />")
-    body.gsub!("\r\n", '<br />')
-    html = "#{html[0..(idx-1)]}#{body}#{html[endidx..(html.size)]}"
-    
-    # images
-    idx = html.index('nss_images')
-    idx = html.index(">", idx)
-    endidx = html.index('</ul>', idx)
-    image_str = html[(idx+1)..(endidx-1)]
-    image_str.strip!
-    image_str.gsub!(',', ' ')
-    imgs = image_str.split(' ')
-    if imgs.size == 0
-      html = remove_section(html, '<section id="thumb-gallery"', '</section>')
-    else
-      imgs.each do |img|
-        img.strip!
-        #next if not img =~ /http/
-        thumb = img.sub("original", "cms_thumb")
-        html.sub!("#{img}", "<li class=\"mTSThumbContainer\"><a rel=\"group1\" class=\"single_image\" href=\"#{img}\"><img class=\"mTSThumb\" src=\"#{thumb}\"/></a></li>")
-      end
-      idx = html.index('nss_images')
-      endidx = html.index("</ul>", idx)
-      hval = html[idx..endidx]
-      hval.gsub!(',', '')
-      html = "#{html[0..(idx-1)]}#{hval}#{html[endidx..-1]}"
-    end
-    
-    # remove published_on
-    html = remove_section(html, '<div class="publish_on">', '</div>')
-         
-    # categories or tags
-    tagstr = tags.join(" ")
-    html.gsub!('$TAGS$', "#{tagstr}")
-    pdate = cms_page.updated_at
-    
-    #  templates
-    html.sub!('$UPDATED_AT$', pdate.strftime("%b-%-d-%Y"))
-    html.sub!('$TITLE$', cms_page.label)
-    html.sub!('$ARTICLEID$', cms_page.id.to_s)
-
-    html
-end
+  def render_page(cms_page)
+    html = convert_page(cms_page)
+    return html
+  end
 
   def index_for_redactor
     tree_walker = ->(page, list, offset) do
--- app/controllers/comfy/cms/content_controller.rb
+++ app/controllers/comfy/cms/content_controller.rb
@@ -1,3 +1,4 @@
+require 'convert_page'
 class Comfy::Cms::ContentController < Comfy::Cms::BaseController
 
   # Authentication module must have `authenticate` method
@@ -27,80 +28,10 @@ class Comfy::Cms::ContentController < Comfy::Cms::BaseController
 
 protected
 
-  def remove_section(html, first, last)
-    idx = html.index(first)
-    return html if idx.nil?
-    endidx = html.index(last, idx) + last.size
-    html = "#{html[0..(idx-1)]}#{html[endidx..(html.size)]}"
-  end
-
   def render_page(status = 200)
     if @cms_layout = @cms_page.layout
       app_layout = (@cms_layout.app_layout.blank? || request.xhr?) ? false : @cms_layout.app_layout
-      
-      # categories to tabs
-      tags_before = @cms_page.categories.map(&:label)
-      tags = []
-      tags_before.each do |tag|
-        tags << tag if not tag =~ /location/
-      end
-      
-      # html
-      html = @cms_page.content_cache
-      
-      # youtube
-      idx = html.index('<div class="video-container">')
-      endidx = html.index('</div>', idx) + '</div>'.size
-      srcidx = html.index('src="', idx) + 'src="'.size
-      endsrcidx = html.index('"', srcidx)
-      if endsrcidx - srcidx < 7
-        html = remove_section(html, '<div class="video-container">', '</div>')
-      end
-      
-      # content
-      idx = html.index('<div class="body_content">') + '<div class="body_content">'.size
-      endidx = html.index('</div>', idx)
-      body = html[(idx+1)..(endidx-1)]
-      body.gsub!("\r\n\r\n", "<p />")
-      body.gsub!("\r\n", '<br />')
-      html = "#{html[0..(idx-1)]}#{body}#{html[endidx..(html.size)]}"
-      
-      # images
-      idx = html.index('nss_images')
-      idx = html.index(">", idx)
-      endidx = html.index('</ul', idx)
-      image_str = html[(idx+1)..(endidx-1)]
-      image_str.strip!
-      image_str.gsub!(',', ' ')
-      imgs = image_str.split(' ')
-      if imgs.size == 0
-        html = remove_section(html, '<section id="thumb-gallery"', '</section>')
-      else
-        imgs.each do |img|
-          img.strip!
-          #next if not img =~ /http/
-          thumb = img.sub("original", "cms_thumb")
-          html.sub!("#{img}", "<li class=\"mTSThumbContainer\"><a rel=\"group1\" class=\"single_image\" href=\"#{img}\"><img class=\"mTSThumb\" src=\"#{thumb}\"/></a></li>")
-        end
-        idx = html.index('nss_images')
-        endidx = html.index("</ul>", idx)
-        hval = html[idx..endidx]
-        hval.gsub!(',', '')
-        html = "#{html[0..(idx-1)]}#{hval}#{html[endidx..-1]}"
-      end
-      
-      # remove published_on
-      html = remove_section(html, '<div class="publish_on">', '</div>')
-           
-      # categories or tags
-      tagstr = tags.join(" ")
-      html.gsub!('$TAGS$', "#{tagstr}")
-      pdate = @cms_page.updated_at
-      
-      #  templates
-      html.sub!('$UPDATED_AT$', pdate.strftime("%b-%-d-%Y"))
-      html.sub!('$TITLE$', @cms_page.label)
-      html.sub!('$ARTICLEID$', @cms_page.id.to_s)
+      html = convert_page(@cms_page)
   
       render  :inline       => html,
               :layout       => app_layout,
